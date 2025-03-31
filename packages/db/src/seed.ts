4d9820b5edcf5659e96b14d0a8a01e8758b31611
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2025-03-31 14:02:18 +0200

    * update deps * disable seeder

--- packages/db/src/seed.ts
+++ packages/db/src/seed.ts
@@ -1,10 +1,11 @@
-import { seed } from "drizzle-seed"
+// import { seed } from "drizzle-seed"
 
-import { db } from "./client"
-import { users } from "./schemas"
+// import { db } from "./client"
+// import { users } from "./schemas"
 
+// currently disabled, since `drizzle-seed` seems not to work with RQB v2
 async function main() {
-  await seed(db, { users }, { count: 10 })
+  //await seed(db, { users }, { count: 10 })
 }
 
 void main()
