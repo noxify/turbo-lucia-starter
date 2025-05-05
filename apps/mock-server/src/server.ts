777ee0e07c420c3d910aeaf7a08ce7de2fec3355
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2025-05-05 17:52:25 +0200

    * use global env also in the mock server * update logging for the mock server * add missing port in the fallback url for the mock server

--- apps/mock-server/src/server.ts
+++ apps/mock-server/src/server.ts
@@ -5,6 +5,5 @@ import { createLogger } from "@acme/logging"
 const logger = createLogger()
 const server = new OAuth2Server()
 await server.issuer.keys.generate("RS256")
-
 await server.start(8080, "localhost")
-logger.info("Issuer URL:", server.issuer.url) // -> http://localhost:8080
+logger.withMetadata({ url: server.issuer.url }).info("Issuer URL:") // -> http://localhost:8080
